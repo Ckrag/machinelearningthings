44f4d2f5ef7d6bd605f14245715aac2ac6f8ca16
longdahl <mikkel.bruunjensen@gmail.com>
2019-06-02 16:42:17 +0200

    apriori with all steps

--- silhouette.py
+++ silhouette.py
@@ -1,54 +1,10 @@
 import math
-import numpy as np
 import pprint
 
+import numpy as np
 
-def centroid(cluster):
-    return (sum([x[0] for x in cluster])) / len(cluster), (sum([y[1] for y in cluster])) / len(cluster)
-
-
-def find_own_cluster(clusters, p):
-    return [x for x in clusters if p in x]
-
-
-def find_other_clusters(clusters, p):
-    return [x for x in clusters if p not in x]
-
-
-def find_mediod(cluster, centroid, dist):
-    return [x for x in (min([dist(p, centroid) for p in cluster]))]
-
-
-def silhouette(clusters, dist):
-    result = []
-    for cluster in clusters:
-        for p in cluster:
-            own_cluster = find_own_cluster(clusters, p)
-            other_clusters = find_other_clusters(clusters, p)
-            other_centroids = [centroid(c) for c in other_clusters]
-            own_cluster_without_self = [x for x in own_cluster[0] if x != p]
-            own_centroid = [centroid(c) for c in [own_cluster_without_self]]
-            dist_to_own_cluster = dist(p, own_centroid[0])
-            dist_to_closest_cluster = min(dist(p, o) for o in other_centroids)
-            result.append((p,
-                           (dist_to_closest_cluster - dist_to_own_cluster) / max(dist_to_own_cluster,
-                                                                                 dist_to_closest_cluster)))
-    pprint.pprint(result)
-
-
-def simple_silhouette(clusters, dist):
-    own_mediod_distances = []
-    other_mediod_distances = []
-    for cluster in clusters:
-        for p in cluster:
-            own_cluster = find_own_cluster(clusters, p)
-            other_clusters = find_other_clusters(clusters, p)
-            other_centroids = [centroid(c) for c in other_clusters]
-            own_centroid = [centroid(c) for c in [own_cluster][0]]
-            own_mediod = (p, dist(own_centroid, p))
-            own_mediod_distances.append(own_mediod)
-        print("CLUSTER 1")
-        pprint.pprint(other_mediod_distances)
+data2 = [[(1, 2), (2, 3), (1, 3)], [(4, 5), (4, 6), (1, 1)], [(10, 12), (11, 12), (14, 17)]]
+data = [[(1, 5), (2, 3), (3, 4), (10, 1)], [(6, 8), (7, 7), (7, 8), (7, 9)]]
 
 
 def euclid(p, o):
@@ -65,6 +21,73 @@ def manhat(p, o):
     return np.sum(abs((np.array(p) - np.array(o))))
 
 
-# silhouette([[(1, 2), (1, 3), (4, 2)], [(5, 6), (4, 7), (8, 4), (4, 5)], [(10, 12), (14, 13), (15, 23)]], manhat)
+def dist_other_cluster():
+    return 1
 
-simple_silhouette([[(4, 2), (1, 3), (4, 2)], [(5, 6), (4, 7), (8, 4), (4, 5)], [(10, 12), (14, 13), (15, 23)]], manhat)
+
+def remove_from_cluster(cluster, p):
+    return [x for x in cluster if x != p]
+
+
+def dist_to_Rest_of_cluster(cluster, p, dist):
+    c = remove_from_cluster(cluster, p)
+    cluster_dist = get_centroid(c)
+    d = dist(p, cluster_dist)
+    return d
+
+
+def get_centroid(cluster):
+    a = sum([x[0] for x in cluster]) / len(cluster)
+    b = sum([x[1] for x in cluster]) / len(cluster)
+
+    return (a, b)
+
+
+def get_min(clusters, dist, p):
+    other_clusters = [x for x in clusters if p not in x]
+    other_centroids = [get_centroid(x) for x in other_clusters]
+    # print(other_centroids)
+    return min([dist(p, x) for x in other_centroids])
+
+
+def silhouette(clusters, dist):
+    for cluster in clusters:
+        for p in cluster:
+            print(p)
+            a = dist_to_Rest_of_cluster(cluster, p, dist)
+            b = get_min(clusters, dist, p)
+            print((b - a) / (max(a, b)))
+
+
+def get_medoid(cluster, dist):
+    centroid = get_centroid(cluster)
+
+    medoid_list = [dist(centroid, p) for p in cluster]
+    # print(medoid_list)
+    # print(medoid_list.index(min(medoid_list)))
+    return cluster[medoid_list.index(min(medoid_list))]
+
+
+def simple_silhouette(clusters, dist, representatives):
+    if representatives == 0:
+        all_medoids = [get_medoid(cluster, dist) for cluster in clusters]
+    else:
+        all_medoids = representatives
+    result = []
+    for i in range(0, len(clusters)):
+        for points in clusters[i]:
+            my_medoid = get_medoid(clusters[i], dist)
+            other_medoids = [x for x in all_medoids if my_medoid != x]
+
+            a = dist(points, my_medoid)
+            b = min([dist(points, x) for x in other_medoids])
+
+            r = (b - a) / (max(a, b))
+            result.append((points, r))
+    pprint.pprint(result)
+
+
+simple_silhouette(data, manhat,
+                  0)  # give representatives as third input. if representatives are not given give 0 as input
+
+# silhouette(data,manhat)
